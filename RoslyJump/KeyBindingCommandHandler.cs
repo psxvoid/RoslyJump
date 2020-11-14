b58f3ca1a66c6035ccaf26ea4deb8c0fd221391c
psxvoid <pavel.sapehin@gmail.com>
2020-11-14 11:02:38 +0300

    Add text endorsement example

---
+++ RoslyJump/KeyBindingCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.Commanding;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Editor.Commanding.Commands;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+
+namespace RoslyJump
+{
+    [Export(typeof(ICommandHandler))]
+    [ContentType("text")]
+    [Name("KeyBindingTest")]
+    class KeyBindingCommandHandler : ICommandHandler<TypeCharCommandArgs>
+    {
+        public string DisplayName => "RoslyJump";
+
+        public bool ExecuteCommand(TypeCharCommandArgs args, CommandExecutionContext executionContext)
+        {
+            if (args.TypedChar == '+')
+            {
+                bool alreadyAdorned = args.TextView.Properties.TryGetProperty(
+                    "KeyBindingTextAdorned", out bool adorned) && adorned;
+                if (!alreadyAdorned)
+                {
+                    new TextAdornment1((IWpfTextView)args.TextView);
+                    args.TextView.Properties.AddProperty("KeyBindingTextAdorned", true);
+                }
+            }
+
+            return false;
+        }
+
+        public CommandState GetCommandState(TypeCharCommandArgs args)
+        {
+            return CommandState.Unspecified;
+        }
+    }
+}
