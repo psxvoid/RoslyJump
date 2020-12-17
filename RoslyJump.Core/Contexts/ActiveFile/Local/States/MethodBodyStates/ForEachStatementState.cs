88c43796e174d94cd99088dd2d037433e3e2ad16
psxvoid <pavel.sapehin@gmail.com>
2020-12-17 07:45:00 +0300

    Add ForEachStatement state

---
+++ RoslyJump.Core/Contexts/ActiveFile/Local/States/MethodBodyStates/ForEachStatementState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using dngrep.core.VirtualNodes;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using RoslyJump.Core.Contexts.ActiveFile.Local.States.BaseStates;
+
+namespace RoslyJump.Core.Contexts.ActiveFile.Local.States.MethodBodyStates
+{
+    public class ForEachStatementState : MethodBodyMemberStateBase<ForEachStatementSyntax>
+    {
+        public ForEachStatementState(LocalContext context, CombinedSyntaxNode contextNode)
+            : base(context, contextNode)
+        {
+        }
+
+        protected override CombinedSyntaxNode[] QueryTargetNodesFunc()
+        {
+            return this.BaseNode.Parent
+                ?.ChildNodes()
+                .Where(x => x.GetType() == typeof(ForEachStatementSyntax))
+                .Select(x => new CombinedSyntaxNode(x))
+                .ToArray()
+                ?? throw new InvalidOperationException(
+                    $"Unable to query target nodes for {nameof(ForEachStatementState)}");
+        }
+    }
+}
