906edd5518a612bc98acefa3dc7c9af1195cd080
psxvoid <pavel.sapehin@gmail.com>
2020-12-22 06:53:06 +0300

    Allow jump down to read-only prop body

--- RoslyJump.Core/Infrastructure/Helpers/CodeAnalysis/MethodMemberParentSyntaxNodeMatcher.cs
+++ RoslyJump.Core/Infrastructure/Helpers/CodeAnalysis/MethodMemberParentSyntaxNodeMatcher.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Linq;
 using dngrep.core.Queries;
+using dngrep.core.VirtualNodes;
+using dngrep.core.VirtualNodes.Syntax;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace RoslyJump.Core.Infrastructure.Helpers.CodeAnalysis
 {
     // TODO: move to dngrep
-    public class MethodMemberParentSyntaxNodeMatcher : ISyntaxNodeMatcher
+    public class MethodMemberParentSyntaxNodeMatcher
+        : ISyntaxNodeMatcher, ICombinedSyntaxNodeMatcher
     {
         private static readonly MethodMemberParentSyntaxNodeMatcher instance =
             new MethodMemberParentSyntaxNodeMatcher();
@@ -20,6 +23,16 @@ namespace RoslyJump.Core.Infrastructure.Helpers.CodeAnalysis
             typeof(LocalFunctionStatementSyntax),
         };
 
+        private readonly static Type[] MaybeMethodMembers = new[]
+        {
+            typeof(PropertyDeclarationSyntax),
+        };
+
+        private readonly static Type[] VirtualMethodMembers = new[]
+        {
+            typeof(ReadOnlyPropertyDeclarationSyntax)
+        };
+
         private MethodMemberParentSyntaxNodeMatcher()
         {
         }
@@ -28,7 +41,41 @@ namespace RoslyJump.Core.Infrastructure.Helpers.CodeAnalysis
         {
             _ = node ?? throw new ArgumentNullException(nameof(node));
 
-            return MethodMembers.Contains(node.GetType());
+            Type nodeType = node.GetType();
+
+            bool isMethodMemberParent = MethodMembers.Contains(nodeType);
+
+            if (isMethodMemberParent)
+            {
+                return true;
+            }
+
+            bool isPotentialMethodMember = MaybeMethodMembers.Contains(nodeType);
+
+            if (!isPotentialMethodMember)
+            {
+                return false;
+            }
+
+            // read-only property with ExpressionBody
+            if (node is PropertyDeclarationSyntax prop
+                && prop.AccessorList == null
+                && prop.ExpressionBody != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool Match(CombinedSyntaxNode node)
+        {
+            if (node.IsVirtual)
+            {
+                return VirtualMethodMembers.Contains(node.MixedNode.GetType());
+            }
+
+            return this.Match(node.BaseNode);
         }
 
         public static MethodMemberParentSyntaxNodeMatcher Instance => instance;
