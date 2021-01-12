98d35bb78186c59a700827bc33202df0b005d351
psxvoid <pavel.sapehin@gmail.com>
2021-01-12 09:18:43 +0300

    Add env vars presence checks
    Additional notes:
    - also enable triggering by any tag

--- pre-release.sh
+++ pre-release.sh
@@ -1,4 +1,17 @@
-if [[ -z $isMarketplaceRelease || "$isMarketplaceRelease" = false ]] ; then
+# to lowercase
+isMarketplaceRelease="${ISMARKETPLACERELEASE,,}"
+
+if [[ -z $isMarketplaceRelease ]]; then
+    >&2 echo 'ENV Error: The environment variable ISMARKETPLACERELEASE is not set.'
+    exit 1
+fi
+
+if ! [[ $isMarketplaceRelease =~ (true|false) ]]; then
+    >&2 echo 'ENV Error: The environment variable ISMARKETPLACERELEASE should be a boolean.'
+    exit 1
+fi
+
+if [[ $isMarketplaceRelease = false ]]; then
     >&2 echo 'Pre-Release Error: The build is not marked as marketplace-ready.'
     exit 1
 fi
