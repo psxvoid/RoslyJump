4738c14ace3a860597ccab2f93574d8a50ff118b
psxvoid <pavel.sapehin@gmail.com>
2021-01-12 18:47:20 +0300

    Multiple changes to version calculation
    Addtional notes:
    - move version calculation to a separate file (pre-build)
    - fix rc tag version retrieval
    - allow release and rc tags only on main branch
    - additional env check (not empty, etc)

--- build.sh
+++ build.sh
@@ -1,30 +1,3 @@
-releaseVersionRegex="^\d+\.\d+\.d+$"
-
-preReleaseVersionRegex="^\d+\.\d+\.d+-rc\d+$"
-
-activeBranch=$(git rev-parse --abbrev-ref HEAD)
-
-releaseGlob="[[:digit:]]*.[[:digit:]]*.[[:digit:]]*"
-releaseGrepRegex="^[[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+$"
-
-# get the latest tag on the current branch
-latestReleaseTag=$(git tag --list $releaseGlob --sort=-creatordate | grep $releaseGrepRegex | head -n 1)
-
-# get the tag before the latest on the current branch
-previousReleaseTag=$(git tag --list $releaseGlob --sort=-creatordate | grep $releaseGrepRegex | head -n 2 | tail -n 1)
-
-# get the latest tag on the main branch
-latestReleaseMainTag=$(git tag --list $releaseGlob --merged main --sort=-creatordate | grep $releaseGrepRegex | head -n 1)
-
-releaseTagOnLatestCommit=$(git tag --contains HEAD --list $releaseGlob | grep $releaseGrepRegex | head -n 1)
-
-isMarketplaceRelease=false
-
-echo $latestReleaseTag
-echo $previousReleaseTag
-echo $latestReleaseMainTag
-# echo $releaseTagOnLatestCommit
-
 if [[ -z $ISPULLREQUEST ]]; then
     >&2 echo 'ENV Error: The environment variable ISPULLREQUEST is not set.'
     exit 1
@@ -32,11 +5,66 @@ fi
 
 isPullRequest="${ISPULLREQUEST,,}"
 
-echo $isPullRequest
-
 if ! [[ $isPullRequest =~ (true|false) ]]; then
     >&2 echo 'ENV Error: The environment variable ISPULLREQUEST is not a boolean.'
     exit 1
+fi
+
+activeBranch=$(git rev-parse --abbrev-ref HEAD)
+
+releaseGlob="[[:digit:]]*.[[:digit:]]*.[[:digit:]]*"
+releaseGrepRegex="^[[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+$"
+releaseCandidateGrepRegex="^[[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+-rc[[:digit:]]\+$"
+
+# get the latest tag on the current branch
+latestReleaseTag=$(git tag --list $releaseGlob --sort=-creatordate | grep $releaseGrepRegex | head -n 1)
+
+# get the tag before the latest on the current branch
+previousReleaseTag=$(git tag --list $releaseGlob --sort=-creatordate | grep $releaseGrepRegex | head -n 2 | tail -n 1)
+
+# get the latest release-candidate tag on the current branch
+latestReleaseCandidateTag=$(git tag --list $releaseGlob --sort=-creatordate | grep $releaseCandidateGrepRegex | head -n 1)
+
+# get the candidate tag before the latest on the current branch
+previousReleaseCandidateTag=$(git tag --list $releaseGlob --sort=-creatordate | grep $releaseCandidateGrepRegex | head -n 2 | tail -n 1)
+
+# get the latest tag on the main branch
+# latestReleaseMainTag=$(git tag --list $releaseGlob --merged main --sort=-creatordate | grep $releaseGrepRegex | head -n 1)
+
+releaseTagOnLatestCommit=$(git tag --contains HEAD --list $releaseGlob | grep $releaseGrepRegex | head -n 1)
+releaseCandidateTagOnLatestCommit=$(git tag --contains HEAD --list $releaseGlob | grep $releaseCandidateGrepRegex | head -n 1)
+
+echo "Latest Release:        ${latestReleaseTag:-empty}"
+echo "Previous Release:      ${previousReleaseTag:-empty}"
+echo "Latest RC:             ${latestReleaseCandidateTag:-empty}"
+echo "Previous RC:           ${previousReleaseCandidateTag:-empty}"
+echo "Latest Commit Release: ${releaseTagOnLatestCommit:-empty}"
+echo "Latest Commit RC:      ${releaseCandidateTagOnLatestCommit:-empty}"
+
+if [[ ! -z $releaseTagOnLatestCommit && ! -z $releaseCandidateTagOnLatestCommit ]]; then
+    >&2 echo "Tag Error: The build can be marked as release or release-candidate (not both)."
+    exit 1
+fi
+
+if [[ $activeBranch != "main" && ! -z $releaseTagOnLatestCommit ]]; then
+    >&2 echo "Tag Error: Marking a build as realease is only allowed on the main branch."
+    exit 1
+fi
+
+if [[ $activeBranch != "main" && ! -z $releaseCandidateTagOnLatestCommit ]]; then
+    >&2 echo "Tag Error: Marking a build as realease-candidate is only allowed on the main branch."
+    exit 1
+fi
+
+isMarketplaceRelease=false
+isReleaseCandidate=false
+
+if [[ $activeBranch = "main" ]]; then
+    if [[ ! -z $releaseTagOnLatestCommit && $isPullRequest = false ]]; then
+        isMarketplaceRelease=true
+    elif [[ ! -z $releaseCandidateTagOnLatestCommit && $isPullRequest = false ]]; then
+        isReleaseCandidate=true
+    fi
 fi
 
 currentMajor=`echo $latestReleaseTag | cut -d. -f1`
@@ -47,11 +75,74 @@ previousMajor=`echo $previousReleaseTag | cut -d. -f1`
 previousMinor=`echo $previousReleaseTag | cut -d. -f2`
 previousPatch=`echo $previousReleaseTag | cut -d. -f3`
 
+latestReleaseCandidateTag="${latestReleaseCandidateTag//-rc/.}"
+currentRcMajor=`echo $latestReleaseCandidateTag | cut -d. -f1`
+currentRcMinor=`echo $latestReleaseCandidateTag | cut -d. -f2`
+currentRcPatch=`echo $latestReleaseCandidateTag | cut -d. -f3`
+currentRc=`echo $latestReleaseCandidateTag | cut -d. -f4`
+
+previousReleaseCandidateTag="${previousReleaseCandidateTag//-rc/.}"
+previousRcMajor=`echo $previousReleaseCandidateTag | cut -d. -f1`
+previousRcMinor=`echo $previousReleaseCandidateTag | cut -d. -f2`
+previousRcPatch=`echo $previousReleaseCandidateTag | cut -d. -f3`
+previousRc=`echo $previousReleaseCandidateTag | cut -d. -f4`
+
+isRcGreater=false
+isRcEqual=false
+
+if (( currentMajor <= currentRcMajor )); then
+    isRcGreater=true
+else
+    if (( currentMinor <= currentRcMinor )); then
+        isRcGreater=true
+    else
+        if (( currentPatch < currentRcPatch )); then
+            isRcGreater=true
+        elif (( currentPatch == currentRcPatch )); then
+            isRcEqual=true
+        fi
+    fi
+fi
+
+isPreviousRcGreater=false
+
+if (( previousMajor <= currentRcMajor )); then
+    isPreviousRcGreater=true
+else
+    if (( previousMinor <= currentRcMinor )); then
+        isPreviousRcGreater=true
+    else
+        if (( previousPatch < currentRcPatch )); then
+            isPreviousRcGreater=true
+        fi
+    fi
+fi
+
+if [[ $isRcGreater = true ]]; then
+    currentMajor=$currentRcMajor
+    currentMinor=$currentRcMinor
+    currentPatch=$currentRcPatch
+fi
+
+if [[ $isPreviousRcGreater = true ]]; then
+    previousMajor=$previousRcMajor
+    previousMinor=$previousRcMinor
+    previousPatch=$previousRcPatch
+fi
+
 if (( currentMajor == previousMajor )); then
     if (( currentMinor == previousMinor )); then
         if (( currentPatch < previousPatch )); then
             >&2 echo "Versioning Error: Patch version is less than the previous one."
             exit 1
+        elif [[ $currentPatch = $previousPatch ]]; then
+            if (( currentRc < previousRc )); then
+                >&2 echo "Versioning Error: Release candidate version is less than the previous one."
+                exit 1
+            elif (( currentRc != previousRc && currentRc != previousRc + 1 )); then
+                >&2 echo "Versioning Error: Release candidate version is increased but not incremented."
+                exit 1
+            fi
         elif (( currentPatch != previousPatch + 1 )); then
             >&2 echo "Versioning Error: Patch version is increased but not incremented."
             exit 1
@@ -75,29 +166,11 @@ else
     fi
 fi
 
-if [[ $activeBranch == "main" || true ]]; then
-    echo main
-    if [[ $releaseTagOnLatestCommit == $latestReleaseMainTag ]]; then
-        # update vsixmanifest version
-        sed -r -i.bak "s/(Identity..*Version=\")([[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*)(\")/\1$currentMajor.$currentMinor.$currentPatch\3/g" ./RoslyJump/source.extension.vsixmanifest
-        isMarketplaceRelease=true
-    else
-        # the "REVISION" environment variable should be set here
-        if [[ -z $REVISION ]]; then
-            >&2 echo 'ENV Error: The REVISION counter environment variable is not set..'
-            exit 1
-        fi
-
-        if ! [[ $REVISION =~ ^[0-9]+$ ]] ; then
-            >&2 echo "ENV Error: The REVISION counter environment variable is not an integer number."
-            exit 1
-        fi
-
-        sed -r -i.bak "s/(Identity..*Version=\")([[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*)(\")/\1$currentMajor.$currentMinor.$currentPatch.$REVISION\3/g" ./RoslyJump/source.extension.vsixmanifest
-    fi
-else
-    # Do Something here
-    echo not main
-fi
+echo "##vso[task.setvariable variable=activeBranch]$activeBranch"
 
 echo "##vso[task.setvariable variable=isMarketplaceRelease]$isMarketplaceRelease"
+echo "##vso[task.setvariable variable=isReleaseCandidate]$isReleaseCandidate"
+
+echo "##vso[task.setvariable variable=latestMajor]$currentMajor"
+echo "##vso[task.setvariable variable=latestMinor]$currentMinor"
+echo "##vso[task.setvariable variable=latestPatch]$currentPatch"
---
+++ pre-build.sh
@@ -0,0 +1,65 @@
+
+activeBranch=$ACTIVEBRANCH
+
+isMarketplaceRelease=${ISMARKETPLACERELEASE,,}
+isReleaseCandidate=${ISRELEASECANDIDATE,,}
+
+latestMajor=$LATESTMAJOR
+latestMinor=$LATESTMINOR
+latestPatch=$LATESTPATCH
+revisionCounter=$REVISIONCOUNTER
+
+if [[ -z $activeBranch ]]; then
+    >&2 echo 'ENV Error: The ACTIVEBRANCH environment variable is not set.'
+    exit 1
+fi
+
+if [[ -z $isMarketplaceRelease ]] || ! [[ $isMarketplaceRelease =~ (true|false) ]]; then
+    >&2 echo 'ENV Error: The ISMARKETPLACERELEASE environment variable is not set.'
+    exit 1
+fi
+
+if [[ -z $isReleaseCandidate ]] || ! [[ $isReleaseCandidate =~ (true|false) ]]; then
+    >&2 echo 'ENV Error: The ISRELEASECANDIDATE environment variable is not set.'
+    exit 1
+fi
+
+if [[ -z $latestMajor ]] || ! [[ $latestMajor =~ ^[0-9]+$ ]]; then
+    >&2 echo 'ENV Error: The LATESTMAJOR environment variable is not set or is not a number.'
+    exit 1
+fi
+
+if [[ -z $latestMinor ]] || ! [[ $latestMinor =~ ^[0-9]+$ ]]; then
+    >&2 echo 'ENV Error: The LATESTMINOR environment variable is not set or is not a number.'
+    exit 1
+fi
+
+if [[ -z $latestPatch ]] || ! [[ $latestPatch =~ ^[0-9]+$ ]]; then
+    >&2 echo 'ENV Error: The LATESTPATCH environment variable is not set or is not a number.'
+    exit 1
+fi
+
+if [[ -z $revisionCounter ]] || ! [[ $revisionCounter =~ ^[0-9]+$ ]]; then
+    >&2 echo 'ENV Error: The REVISIONCOUNTER environment variable is not set or is not a number.'
+    exit 1
+fi
+
+releaseVersion="$latestMajor.$latestMinor.$latestPatch.$revisionCounter"
+
+# update vsixmanifest version
+if [[ $activeBranch == "main" && $isMarketplaceRelease = true ]]; then
+    releaseVersion="$latestMajor.$latestMinor.$latestPatch"
+fi
+
+sed -r -i.bak "s/(Identity..*Version=\")([[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*)(\")/\1$releaseVersion\3/g" ./RoslyJump/source.extension.vsixmanifest
+
+# verify the version number is correctly updated in the vsixmanifest
+grep -Eq "Identity.*Version=\"$releaseVersion\"" ./RoslyJump/source.extension.vsixmanifest \
+    && success=true || success=false
+
+if [[ $success = false ]]; then
+    >&2 echo 'Pre-build Error: Unable to update VSIX version number.'
+    exit 1
+fi
+
+echo "##vso[task.setvariable variable=releaseVersion]$releaseVersion"
