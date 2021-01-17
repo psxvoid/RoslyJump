dc1fbf14dccb8390510c15c6a8c520f1bcfa9373
psxvoid <pavel.sapehin@gmail.com>
2021-01-17 06:13:56 +0300

    Fix main and tag branch detection, add output

--- pre-build-phase-1.sh
+++ pre-build-phase-1.sh
@@ -10,7 +10,15 @@ if ! [[ $isPullRequest =~ (true|false) ]]; then
     exit 1
 fi
 
-activeBranch=$(git rev-parse --abbrev-ref HEAD)
+if [[ -z $BUILD_SOURCEBRANCH ]]; then
+    >&2 echo 'ENV Error: The environment variable BUILD_SOURCEBRANCH is not set.'
+    exit 1
+fi
+
+# refs/heads/master
+# refs/pull/1/merge
+# refs/tags/your-tag-name
+activeBranch=$BUILD_SOURCEBRANCH
 
 releaseGlob="[[:digit:]]*.[[:digit:]]*.[[:digit:]]*"
 releaseGrepRegex="^[[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+$"
@@ -28,8 +36,11 @@ latestReleaseCandidateTag=$(git tag --list $releaseGlob --sort=-creatordate | gr
 # get the candidate tag before the latest on the current branch
 previousReleaseCandidateTag=$(git tag --list $releaseGlob --sort=-creatordate | grep $releaseCandidateGrepRegex | head -n 2 | tail -n 1)
 
-# get the latest tag on the main branch
-# latestReleaseMainTag=$(git tag --list $releaseGlob --merged main --sort=-creatordate | grep $releaseGrepRegex | head -n 1)
+# get the latest release tag on the main branch
+latestReleaseMainTag=$(git tag --list $releaseGlob --merged main --sort=-creatordate | grep $releaseGrepRegex | head -n 1)
+
+# get the latest release-candidate tag on the main branch
+latestReleaseCandidateMainTag=$(git tag --list $releaseGlob --merged main --sort=-creatordate | grep $releaseCandidateGrepRegex | head -n 1)
 
 releaseTagOnLatestCommit=$(git tag --contains HEAD --list $releaseGlob | grep $releaseGrepRegex | head -n 1)
 releaseCandidateTagOnLatestCommit=$(git tag --contains HEAD --list $releaseGlob | grep $releaseCandidateGrepRegex | head -n 1)
@@ -38,34 +49,52 @@ echo "Latest Release:        ${latestReleaseTag:-empty}"
 echo "Previous Release:      ${previousReleaseTag:-empty}"
 echo "Latest RC:             ${latestReleaseCandidateTag:-empty}"
 echo "Previous RC:           ${previousReleaseCandidateTag:-empty}"
+echo "Latest Main Release:   ${latestReleaseMainTag:-empty}"
+echo "Latest Main RC:        ${latestReleaseCandidateMainTag:-empty}"
 echo "Latest Commit Release: ${releaseTagOnLatestCommit:-empty}"
 echo "Latest Commit RC:      ${releaseCandidateTagOnLatestCommit:-empty}"
+echo "Active Branch:         $activeBranch"
 
 if [[ ! -z $releaseTagOnLatestCommit && ! -z $releaseCandidateTagOnLatestCommit ]]; then
     >&2 echo "Tag Error: The build can be marked as release or release-candidate (not both)."
     exit 1
 fi
 
-if [[ $activeBranch != "main" && ! -z $releaseTagOnLatestCommit ]]; then
-    >&2 echo "Tag Error: Marking a build as realease is only allowed on the main branch."
-    exit 1
-fi
-
-if [[ $activeBranch != "main" && ! -z $releaseCandidateTagOnLatestCommit ]]; then
-    >&2 echo "Tag Error: Marking a build as realease-candidate is only allowed on the main branch."
-    exit 1
-fi
-
 isMarketplaceRelease=false
 isReleaseCandidate=false
 
-if [[ $activeBranch = "main" ]]; then
-    if [[ ! -z $releaseTagOnLatestCommit && $isPullRequest = false ]]; then
-        isMarketplaceRelease=true
-    elif [[ ! -z $releaseCandidateTagOnLatestCommit && $isPullRequest = false ]]; then
-        isReleaseCandidate=true
+if [[ ! -z $releaseTagOnLatestCommit || ! -z $releaseCandidateTagOnLatestCommit ]]; then
+    if [[ $activeBranch = "refs/heads/main" || $activeBranch = "refs/tags/$releaseTagOnLatestCommit" || $activeBranch = "refs/tags/$releaseCandidateTagOnLatestCommit" ]]; then
+        if [[ ! -z $releaseTagOnLatestCommit && $releaseTagOnLatestCommit = $latestReleaseMainTag && $isPullRequest = false ]]; then
+            # this is release-tag branch, e.g. tags/1.0.0 (azure pipelines tag trigger)
+            # or main branch with the latest release-tag (azure pipelines manual trigger)
+            isMarketplaceRelease=true
+        elif [[ ! -z $releaseTagOnLatestCommit && ! $releaseTagOnLatestCommit = $latestReleaseMainTag ]]; then
+            >&2 echo "Tag Error: Marking a build as realease is only allowed on the main branch (a tag branch but version is not equal to main)."
+            exit 1
+        fi
+
+        if [[ ! -z $releaseCandidateTagOnLatestCommit && $releaseCandidateTagOnLatestCommit = $latestReleaseCandidateMainTag && $isPullRequest = false ]]; then
+            isReleaseCandidate=true
+        elif [[ ! -z $releaseCandidateTagOnLatestCommit && ! $releaseCandidateTagOnLatestCommit = $latestReleaseCandidateMainTag ]]; then
+            >&2 echo "Tag Error: Marking a build as realease-candidate is only allowed on the main branch (a tag branch but version is not equal to main)."
+            exit 1
+        fi
+    else
+        if [[ ! -z $releaseTagOnLatestCommit ]]; then
+            >&2 echo "Tag Error: Marking a build as realease is only allowed on the main branch (not main or tag branch)."
+            exit 1
+        fi
+
+        if [[ ! -z $releaseCandidateTagOnLatestCommit ]]; then
+            >&2 echo "Tag Error: Marking a build as realease-candidate is only allowed on the main branch (not main or tag branch)."
+            exit 1
+        fi
     fi
 fi
+
+echo "Is Marketplace-Ready: $isMarketplaceRelease"
+echo "Is Release Candidate: $isReleaseCandidate"
 
 currentMajor=`echo $latestReleaseTag | cut -d. -f1`
 currentMinor=`echo $latestReleaseTag | cut -d. -f2`
@@ -74,6 +103,14 @@ currentPatch=`echo $latestReleaseTag | cut -d. -f3`
 previousMajor=`echo $previousReleaseTag | cut -d. -f1`
 previousMinor=`echo $previousReleaseTag | cut -d. -f2`
 previousPatch=`echo $previousReleaseTag | cut -d. -f3`
+
+echo "Current Major: $currentMajor"
+echo "Current Minor: $currentMinor"
+echo "Current Patch: $currentPatch"
+
+echo "Previous Major: $previousMajor"
+echo "Previous Minor: $previousMinor"
+echo "Previous Patch: $previousPatch"
 
 latestReleaseCandidateTag="${latestReleaseCandidateTag//-rc/.}"
 currentRcMajor=`echo $latestReleaseCandidateTag | cut -d. -f1`
@@ -86,6 +123,14 @@ previousRcMajor=`echo $previousReleaseCandidateTag | cut -d. -f1`
 previousRcMinor=`echo $previousReleaseCandidateTag | cut -d. -f2`
 previousRcPatch=`echo $previousReleaseCandidateTag | cut -d. -f3`
 previousRc=`echo $previousReleaseCandidateTag | cut -d. -f4`
+
+echo "Current RC Major: $currentMajor"
+echo "Current RC Minor: $currentMinor"
+echo "Current RC Patch: $currentPatch"
+
+echo "Previous RC Major: $previousMajor"
+echo "Previous RC Minor: $previousMinor"
+echo "Previous RC Patch: $previousPatch"
 
 isRcGreater=false
 isRcEqual=false
@@ -104,6 +149,9 @@ else
     fi
 fi
 
+echo "Is RC Greater: $isRcGreater"
+echo "Is RC Equal  : $isRcEqual"
+
 isPreviousRcGreater=false
 
 if (( previousMajor <= currentRcMajor )); then
@@ -118,6 +166,8 @@ else
     fi
 fi
 
+echo "Is Previous RC Greater: $isPreviousRcGreater"
+
 if [[ $isRcGreater = true ]]; then
     currentMajor=$currentRcMajor
     currentMinor=$currentRcMinor
@@ -129,6 +179,9 @@ if [[ $isPreviousRcGreater = true ]]; then
     previousMinor=$previousRcMinor
     previousPatch=$previousRcPatch
 fi
+
+echo "Current Version : $currentMajor.$currentMinor.$currentPatch"
+echo "Previous Version: $previousMajor.$previousMinor.$previousPatch"
 
 if (( currentMajor == previousMajor )); then
     if (( currentMinor == previousMinor )); then
