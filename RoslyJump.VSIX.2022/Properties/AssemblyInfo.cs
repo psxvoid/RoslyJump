30ce54bf42cfe38197ee5b3c869f9af02baa5a93
psxvoid <pavel.sapehin@gmail.com>
2023-11-04 18:11:44 +0100

    Rename assebly attributes for VSIX projects

--- RoslyJump.VSIX.2022/Properties/AssemblyInfo.cs
+++ RoslyJump.VSIX.2022/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RoslyJump.VSIX._2022")]
+[assembly: AssemblyTitle("RoslyJump.VSIX_2022")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RoslyJump.VSIX._2022")]
+[assembly: AssemblyProduct("RoslyJump.VSIX_2022")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
