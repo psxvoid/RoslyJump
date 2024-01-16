a1f97ad40d25edad0ed5f986cfb842249467466f
psxvoid <pavel.sapehin@gmail.com>
2024-01-16 08:33:48 +0100

    Extract IActiveViewAccessor + fix namespace

---
+++ RoslyJump.Shared/Infrastructure/VisualStudio/TextEditor/IActiveViewAccessor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.VisualStudio.Text.Editor;
+
+namespace RoslyJump.Shared.Infrastructure.VisualStudio.TextEditor
+{
+    public interface IActiveViewAccessor
+    {
+        IWpfTextView? ActiveView { get; }
+    }
+}
